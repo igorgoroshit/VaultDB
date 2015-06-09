7d97106159c8a2dcc2b89e46346a4ebb9a4de56e
maxlabelle <maxime_labelle@hotmail.com>
2015-06-09 13:12:56 -0400

    .

--- docs/__footer.php
+++
@@ -1,8 +0,0 @@
-			<br/>
-	<div class="footer">
-        <p align="center"><strong>&copy; Maxime Labelle 2009-<?php print date("Y"); ?></strong></p>
-      </div>
-
-	</div> <!-- /container -->
-</body>
-</html>
